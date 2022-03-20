5fc2e8cf853b6ed48f41f78851b35ed6dfd71b20
Louka Danau <louka.danau@student.odisee.be>
2022-03-20 17:35:53 +0100

    Add project files.

---
+++ opgave 7_optelling/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Count: ");
+int count = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+
+if (count == 1)
+{
+    Console.WriteLine("1");
+}
+else
+{
+    int[] number = new int[count];
+    number[0] = 1;
+    number[1] = 1;
+
+    for (int i = 2; i < count; i++)
+    {
+        number[i] = number[i - 1] + number[i - 2];
+    }
+        
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(number[i] + " ");
+    }   
+}
+Console.WriteLine("");
